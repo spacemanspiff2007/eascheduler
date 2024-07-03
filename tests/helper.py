66ac97b1b94deb64f0babcbc94254279781395dd
spacemanspiff2007 <10754716+spacemanspiff2007@users.noreply.github.com>
2024-07-03 13:38:17 +0200

    .

--- tests/helper.py
+++ tests/helper.py
@@ -5,11 +5,6 @@ def get_local_as_utc(month=1, day=1, hour=0, minute=0, second=0, *, year=2001,) 
     return LocalSystemDateTime(year, month, day, hour, minute=minute, second=second).as_utc()
 
 
-def get_local_as_utc(month=1, day=1, hour=0, minute=0, second=0, *, year=2001, microsecond=0) -> UTCDateTime:
-    return ZonedDateTime(
-        year, month, day, hour, minute=minute, second=second, microsecond=microsecond, tz='Europe/Berlin').as_utc()
-
-
 def cmp_utc_with_local(obj, *args, **kwargs):
     from_local = get_local_as_utc(*args, **kwargs)
     assert obj == from_local, f"\n{obj}\n{from_local}"
