f3364c697e411ddb59d71447bc0a0452cc364c94
spacemanspiff2007 <10754716+spacemanspiff2007@users.noreply.github.com>
2024-09-23 14:23:48 +0200

    .

--- src/eascheduler/__init__.py
+++ src/eascheduler/__init__.py
@@ -1,6 +1,13 @@
 from eascheduler import errors
 from eascheduler.__version__ import __version__
-from eascheduler.builder import get_sun_position, is_holiday
+from eascheduler.builder import (
+    add_holiday,
+    get_holiday_name,
+    get_holidays_by_name,
+    get_sun_position,
+    is_holiday,
+    pop_holiday,
+)
 from eascheduler.errors.handler import set_exception_handler
 from eascheduler.producers.prod_filter_holiday import setup_holidays
 from eascheduler.producers.prod_sun import set_location
